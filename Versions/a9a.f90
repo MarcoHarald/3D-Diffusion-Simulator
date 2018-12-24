34955b811720eee067f963df794a7a15947f265d
Marco Harald <araldus99@gmail.com>
2018-12-24 14:59:57 +0100

    Debugged rounding error micro-simulation bug

--- Versions/a9a.f90
+++ Versions/a9a.f90
@@ -6,7 +6,7 @@ INTEGER :: x,y,z,i,k, steps, flag
 REAL :: xAxis(-x0:x0),yAxis(-y0:y0),zAxis(-z0:z0)
 REAL :: dx_temp(-x0:x0,-y0:y0,-z0:z0),dy_temp(-x0:x0,-y0:y0,-z0:z0),dz_temp(-x0:x0,-y0:y0,-z0:z0)
 REAL :: tolerance, dt_temp(-x0:x0,-y0:y0,-z0:z0) !temp(-x0:x0,-y0:y0,-z0:z0)
-REAL :: c_dfsn,c_r,d_r,d_t, temp0, temp1,t_siml, bound
+REAL :: constDfsn,initRad,stepDist,stepTime, temp0, temp1,t_siml, bound, initRad2
 
 !4-dimensional array of REAL with size to be allocated
 REAL, DIMENSION(:,:,:,:), ALLOCATABLE :: array
@@ -21,10 +21,10 @@ INTEGER :: x_position,y_position,z_position, t_frame, stepSize
 !t_frame		snapshot of instance in time
 
 
-!c_dfsn		diffusion constant (cm2 s-1)
-!c_r		radius of initial (cm)
-!d_r		space step size (cm)
-!d_t		time step size (s)
+!constDfsn		constant of diffusion  (cm2 s-1)
+!initRad		initial radius (cm)
+!stepDist		space step size (cm)
+!stepTime		time step size (s)
 
 !	***ACTION***	change variable names for demonstrators
 !			full width half maximum
@@ -34,26 +34,18 @@ INTEGER :: x_position,y_position,z_position, t_frame, stepSize
 !----------------END OF REFERENCE-----------------
 
 
-	c_dfsn = 1.5
-	c_r = 2.0
-	d_r = 1.0
-	d_t = 1.0e-1
+	constDfsn = 1.1
+	initRad = 2.0
+	initRad2 = 4.0
+	stepDist = 1.0
+	stepTime = 1.0e-1
 
 	!CONNECT FILES FOR INPUT/OUTPUT
-	OPEN(UNIT=12, FILE="prelim.dat")
- 	OPEN(UNIT=13, FILE="sampling.dat")
-	OPEN(UNIT=14, FILE="endVal.dat")
-	OPEN(UNIT=15, FILE="diffs.dat")
-	OPEN(UNIT=16, FILE="endVis.dat")
-	OPEN(UNIT=17, FILE="slice1.dat")
-	OPEN(UNIT=18, FILE="slice2.dat")
-	OPEN(UNIT=19, FILE="slice3.dat")
-	OPEN(UNIT=22, FILE="et1.dat")
-	OPEN(UNIT=23, FILE="et2.dat")
-	OPEN(UNIT=24, FILE="et3.dat")
-	OPEN(UNIT=25, FILE="et4.dat")
-	OPEN(UNIT=26, FILE="et5.dat")
-	OPEN(UNIT=30, FILE="initVal.dat")
+	OPEN(UNIT=12, FILE="initAll.dat")
+	OPEN(UNIT=13, FILE="endAll.dat")
+	OPEN(UNIT=14, FILE="initSlice.dat")
+	OPEN(UNIT=15, FILE="endSlice.dat")
+	OPEN(UNIT=30, FILE="val0.dat")
 	OPEN(UNIT=31, FILE="val1.dat")
 	OPEN(UNIT=32, FILE="val2.dat")
 	OPEN(UNIT=33, FILE="val3.dat")
@@ -66,27 +58,27 @@ WRITE(6,*) 'CHECK   1'
 tolerance = 10.0**(-5.0)
 
 !SET INITIAL GUESS AT BOUNDARY CONDITIONS (BC)
-temp1 = 10.0
-temp0 = 20.0
+temp1 = 20.0
+temp0 = 100.0
 
 !SET SIMULATION LENGTH (s)
 t_siml = 10.0
 
 !DEFINE EXACT VALUE OF COORDINATES ON THREE AXES
 DO i=-x0,x0
-xAxis(i)=REAL(i)*d_r
+	xAxis(i)=REAL(i)*stepDist
 END DO
 
 DO i=-y0,y0
-yAxis(i)=REAL(i)*d_r
+	yAxis(i)=REAL(i)*stepDist
 END DO
 
 DO i=-z0,z0
-zAxis(i)=REAL(i)*d_r
+	zAxis(i)=REAL(i)*stepDist
 END DO
 
 !INITIATE STEPS VARIABLE
-steps = int(real(t_siml)/d_t)
+steps = int(real(t_siml)/stepTime)
 WRITE(6,*) '# OF STEPS: ', steps
 
 !DETERMINE ALLOCATABLE ARRAY DIMENSIONS
@@ -101,25 +93,30 @@ WRITE(6,*)'Y-AXIS WIDTH: ', y0
 WRITE(6,*)'Z-AXIS WIDTH: ', z0
 !----------------------------------------------
 
-ALLOCATE(array(-x_position:x_position,-y_position:y_position,-z_position:z_position,1:t_frame)) 
+ALLOCATE(array(-x_position:x_position,-y_position:y_position,-z_position:z_position,0:t_frame)) 
 !DEALLOCATE(array)
-
 
 !***********************
 WRITE(6,*) 'CHECK   2'
 !***********************
-
 
 !CHECK IF COORDINATE IS INSIDE INITIAL CONDITION & SET IT TO BC
 !INITIALISE FIRST 'ARRAY' VALUES (INITIAL CONDITIONS)
 DO z=-z0,z0
 	DO y=-y0,y0	
 		DO x=-x0,x0
-			IF(abs(xAxis(x))<c_r+tolerance .AND. abs(yAxis(y))<c_r+tolerance .AND. abs(zAxis(z))<c_r+tolerance) THEN
-					array(x,y,z,1)=temp0
-				ELSE
-					array(x,y,z,1)=temp1
-			END IF 
+	!		IF(abs(xAxis(x))<initRad+tolerance .AND. abs(yAxis(y))<initRad+tolerance .AND. abs(zAxis(z))<initRad+tolerance) THEN
+	!			array(x,y,z,0)=temp0
+	!			WRITE(6,*) 'inital conditions coord',X,Y,Z
+			IF((xAxis(x))>x0-initRad2+tolerance .AND. (yAxis(y))>y0-initRad2+tolerance .AND. (zAxis(z))>z0-initRad2+tolerance) THEN
+				array(x,y,z,0)=temp0
+				WRITE(6,*) 'inital conditions coord',X,Y,Z
+			ELSEIF((xAxis(x))<-x0+initRad2+tolerance .AND. (yAxis(y))<-y0+initRad2+tolerance .AND. (zAxis(z))>z0-initRad2+tolerance) THEN
+				array(x,y,z,0)=temp0
+				WRITE(6,*) 'inital conditions coord',X,Y,Z
+			ELSE
+				array(x,y,z,0)=temp1
+			ENDIF 
 		END DO
 	END DO
 END DO
@@ -128,54 +125,19 @@ END DO
 WRITE(6,*) 'CHECK  3'
 !***********************
 
-
-!OUTPUT:initial values (loop cycles over x-position & then y-position)
-DO z=-z0,z0
-	DO y=-y0,y0	
-		DO x=-x0,x0	
-		WRITE(12,*) xAxis(x),yAxis(y),zAxis(z),array(x,y,z,1)
-		END DO 
-	END DO
-	WRITE(12,*)
-END DO
-
-!OUTPUT:initial values for orthogonal planes on each axes (set x,y,z to 0 sequentially)
-	z=0	
-	DO y=1-y0,y0-1	
-		DO x=1-x0,x0-1
-			WRITE(30,*) xAxis(x), yAxis(y),zAxis(z),array(x,y,z,1)
-		END DO 
-		WRITE(30,*)
-	END DO
-	WRITE(30,*)
-
-	bound = 4.0
-
-	DO x=int(-x0+x0/bound),int(x0-1),int(2*x0/bound)	
-		DO z=0,z0-1
-			DO y=1-y0,y0-1
-				WRITE(30,*) xAxis(x), yAxis(y),zAxis(z),array(x,y,z,1)
-			END DO
-			WRITE(30,*)
-		END DO
-		WRITE(30,*)
-	END DO
-	WRITE(30,*)
-
-
 !***********************
 WRITE(6,*) 'CHECK  3A'
 !***********************
 
-!LOOP OVER INSTANCES OF TIME (FRAMES)
-DO k=1, steps-1
+!LOOP OVER INSTANCES OF TIME (FRAMES) (set to steps-1] to avoid bug)
+DO k=0, steps-1
 !GRADIENT OF TEMP CHANGE WITH RESPECT TO TIME [ x ]
 	DO z=1-z0,z0-1
 		DO y=1-y0,y0-1	
 			DO x=1-x0,x0-1
-				dx_temp(x,y,z) = ((array(x+1,y,z,k))-(2.0*array(x,y,z,k))+(array(x-1,y,z,k)))/(d_r*d_r)
-				dy_temp(x,y,z) = ((array(x,y+1,z,k))-(2.0*array(x,y,z,k))+(array(x,y-1,z,k)))/(d_r*d_r)
-				dZ_temp(x,y,z) = ((array(x,y,z+1,k))-(2.0*array(x,y,z,k))+(array(x,y,z-1,k)))/(d_r*d_r)			
+				dx_temp(x,y,z) = ((array(x+1,y,z,k))-(2.0*array(x,y,z,k))+(array(x-1,y,z,k)))/(stepDist*stepDist)
+				dy_temp(x,y,z) = ((array(x,y+1,z,k))-(2.0*array(x,y,z,k))+(array(x,y-1,z,k)))/(stepDist*stepDist)
+				dZ_temp(x,y,z) = ((array(x,y,z+1,k))-(2.0*array(x,y,z,k))+(array(x,y,z-1,k)))/(stepDist*stepDist)					
 			END DO
 		END DO
 	END DO
@@ -184,8 +146,13 @@ DO k=1, steps-1
 	DO z=1-z0,z0-1
 		DO y=1-y0,y0-1	
 			DO x=1-x0,x0-1
-				dt_temp(x,y,z) = c_dfsn*dx_temp(x,y,z)+c_dfsn*dy_temp(x,y,z)+c_dfsn*dz_temp(x,y,z)
-				array(x,y,z,k+1) = array(x,y,z,k) + (d_t*dt_temp(x,y,z))
+				dt_temp(x,y,z) = constDfsn*dx_temp(x,y,z)+constDfsn*dy_temp(x,y,z)+constDfsn*dz_temp(x,y,z)
+				IF(dt_temp(z,y,z)>1.0E-24) THEN
+					array(x,y,z,k+1) = array(x,y,z,k) + (stepTime*dt_temp(x,y,z))
+					WRITE(6,*) k, dt_temp(x,y,z)
+				ELSE
+					array(x,y,z,k+1) = array(x,y,z,k)
+				ENDIF
 			END DO
 		END DO
 	END DO
@@ -202,73 +169,51 @@ WRITE(6,*) 'CHECK   4'
 WRITE(6,*) 'CHECK   5'
 !***********************
 
-stepSize = 4
-!OUTPUT:end values for all 3D space (loop cycles over x-position & then y-position)
-	DO z=1-z0,z0-1-stepSize, stepSize
-		DO y=1-y0,y0-1	
-			DO x=1-x0,x0-1
-				WRITE(14,*) xAxis(x), yAxis(y),zAxis(z),array(x,y,z,steps-1)
-			END DO 
-			WRITE(14,*)
-		END DO
-		WRITE(14,*)
-	END DO
-
-!OUTPUT:end values for 3 orthogonal planes on each axes (set x,y,z to 0 sequentially)
-	z=0	
-		DO y=1-y0,y0-1	
-			DO x=1-x0,x0-1
-				WRITE(16,*) xAxis(x), yAxis(y),zAxis(z),array(x,y,z,steps-1)
-				WRITE(17,*) xAxis(x), yAxis(y),zAxis(z),array(x,y,z,steps-1)
-			END DO 
-			WRITE(16,*)
-			WRITE(17,*)
-		END DO
-WRITE(16,*)
-
-	y=0
-		DO z=0,z0-1
-			DO x=0,x0-1
-				!WRITE(16,*) xAxis(x), yAxis(y),zAxis(z),array(x,y,z,steps-1)
-				WRITE(18,*) zAxis(z),xAxis(x), yAxis(y),array(x,y,z,steps-1)
-			END DO 
-			WRITE(16,*)
-			WRITE(18,*)
-		END DO
-WRITE(16,*)
-	
-bound = 4.0
-	DO x=int(-x0+x0/bound),int(x0-1),int(2*x0/bound)	
-		DO z=0,z0-1
-			DO y=1-y0,y0-1
-				WRITE(16,*) xAxis(x), yAxis(y),zAxis(z),array(x,y,z,steps-1)
-				WRITE(19,*) yAxis(y),zAxis(z),xAxis(x),array(x,y,z,steps-1)
+!OUTPUT: all initial & end values (loop cycles over x-position & then y-position)
+	DO k=0,1
+		DO z=1-z0,z0-1
+			DO y=1-y0,y0-1	
+				DO x=1-x0,x0-1	
+				WRITE(12+k,*) xAxis(x),yAxis(y),zAxis(z),array(x,y,z,(k*steps))
+				END DO 
+				WRITE(12+k,*)
 			END DO
-			WRITE(16,*)
-			WRITE(19,*)
+			WRITE(12+k,*)
 		END DO
-WRITE(16,*)
+		WRITE(12+k,*)
 	END DO
-WRITE(16,*)
 
+!OUTPUT:initial & end values for horizonal slices 3D space (loop cycles over x-position & then y-position)
+	stepSize = 4
+	DO k=0,1
+		DO z=1-z0,z0-1-stepSize, stepSize
+			DO y=1-y0,y0-1	
+				DO x=1-x0,x0-1
+					WRITE(14+k,*) xAxis(x), yAxis(y),zAxis(z),array(x,y,z,(k*steps))
+				END DO 
+				WRITE(14+k,*)
+			END DO
+			WRITE(14+k,*)
+		END DO
+	END DO
 
-!OUTPUT:temperature distribution for a slice of the 3D space, over time (loop 'samples' the data every 1000th value to save memory)
-	DO k=1,3
+!OUTPUT:temperature distribution for a slice of the 3D space, over time (loop creates frames in time - 'samples' the data)
+	flag = 3
+	DO k=0,flag
 		z=0	
 		DO y=1-y0,y0-1	
 			DO x=1-x0,x0-1
-				WRITE(30+k,*) xAxis(x), yAxis(y),zAxis(z),array(x,y,z,int(k*steps/3))
+				WRITE(30+k,*) xAxis(x), yAxis(y),zAxis(z),array(x,y,z,int(k*steps/flag))
 			END DO 
 			WRITE(30+k,*)
 		END DO
 		WRITE(30+k,*)
 
 		bound = 4.0
-
 		DO x=int(-x0+x0/bound),int(x0-1),int(2*x0/bound)	
 			DO z=0,z0-1
 				DO y=1-y0,y0-1
-					WRITE(30+k,*) xAxis(x), yAxis(y),zAxis(z),array(x,y,z,int(k*steps/3))
+					WRITE(30+k,*) xAxis(x), yAxis(y),zAxis(z),array(x,y,z,int(k*steps/flag))
 				END DO
 				WRITE(30+k,*)
 			END DO
@@ -280,42 +225,6 @@ WRITE(16,*)
 !***********************
 WRITE(6,*) 'CHECK   6'
 !***********************
-
-
-!--------------------------------------------------
-!OUTPUT:slice of final values (loop cycles slices of the z axis)
-stepSize = 4
-!OUTPUT:end values for all 3D space (loop cycles over x-position & then y-position)
-DO i=1, 5
-!define bounds between which points must lie to be included in the isocline
-!                  step      starting lower bound
-	bound = real(i)*0.3+0.5
-	DO z=1-z0,z0-1
-		DO y=1-y0,y0-1	
-			DO x=1-x0,0
-				flag = 0				
-				
-				!IF(array(x,y,z,steps-1) > bound .AND. array(x,y,z,steps-1)<(bound+0.2)) THEN
-				IF(array(x,y,z,steps-1) < bound) THEN
-					WRITE(22+i,*) xAxis(x), yAxis(y),zAxis(z),array(x,y,z,steps-1)
-					WRITE(6,*) xAxis(x), yAxis(y),zAxis(z),array(x,y,z,steps-1)
-					flag = 1
-				ELSE
-				END IF
-			END DO 
-			!IF (flag == 1) THEN
-			!	WRITE(22+i,*)
-			!END IF
-		END DO
-			IF (flag == 1) THEN
-				WRITE(22+i,*)
-			END IF
-	END DO
-END DO
-
-
-!---------------------------------
-
 
 END PROGRAM pdes
 
--- Versions/script2.gp
+++
@@ -1,8 +0,0 @@
-set output "filename.ps"
-set size xscale[-10,10],yscale[-10,10]
-set xlabel "x axis"
-set tics out
-set ticslevel 10
-splot "endVal.dat" every ::1::361 using 1:2:3 with lines
-
-
